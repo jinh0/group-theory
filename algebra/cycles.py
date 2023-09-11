c089cc499c0490d7c9954ff66b9d28ebb8266212
Jinho Yoon <yoonjinho0318@gmail.com>
2023-09-10 22:38:12 -0400

    refactor: remove length parameter

--- algebra/cycles.py
+++ algebra/cycles.py
@@ -2,15 +2,14 @@ import itertools
 
 Perm = dict[int, int]
 
-def str_to_perm(perm_str: str, n: int) -> Perm:
-    cycles = perm_str[1:-1].split(')(')
-    return prod(
-        [parse_cycle(cycle, n) for cycle in cycles], n
-    )
+def str_to_perm(perm_str: str) -> Perm:
+    cycles = [parse_cycle(x) for x in perm_str[1:-1].split(')(')]
+    return prod(cycles)
 
-def parse_cycle(cycle_str: str, n: int) -> Perm:
+def parse_cycle(cycle_str: str) -> Perm:
     elems = list(map(int, cycle_str.split(' ')))
-    perm = {i: i for i in range(1, n + 1)}
+
+    perm = {}
     for i in range(len(elems) - 1):
         perm[elems[i]] = elems[i + 1]
 
@@ -18,21 +17,25 @@ def parse_cycle(cycle_str: str, n: int) -> Perm:
 
     return perm
 
-def prod(cycles: list[Perm], n: int):
-    res = {i: i for i in range(1, n + 1)}
+def prod(cycles: list[Perm]):
+    res = {}
     for cycle in reversed(cycles):
         for x in res:
-            res[x] = cycle[res[x]]
+            if res[x] in cycle:
+                res[x] = cycle[res[x]]
 
         for x in set(cycle.keys()) - set(res.keys()):
             res[x] = cycle[x]
 
     return res
 
+# NOTE: The cycles in the string are sorted!!!
 def perm_to_str(perm: Perm):
     # Cycle detection DFS
     cycles: list[list[int]] = []
     visited = set()
+
+    perm = dict(sorted(perm.items()))
 
     def dfs(x, cycle):
         if x in visited:
@@ -54,8 +57,8 @@ def perm_to_str(perm: Perm):
     stringify = lambda l : ' '.join(map(str, l))
     return '(' + ')('.join(map(stringify, n_cycles)) + ')'
 
-def simplify(perm_str: str, n: int) -> str:
-    return perm_to_str(str_to_perm(perm_str, n))
+def evaluate(perm_str: str) -> str:
+    return perm_to_str(str_to_perm(perm_str))
 
 def s(n: int) -> list[Perm]:
     res = []
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from hypothesis import given, strategies as st
-from algebra.cycles import str_to_perm, perm_to_str, simplify
+from algebra.cycles import str_to_perm, perm_to_str, evaluate
 
 @st.composite
 def gen_perm(draw):
@@ -13,12 +13,17 @@ def gen_perm(draw):
 
     return dict(zip(range(1, n + 1), xs))
 
+# Without the number of elements, in order to compare permutations,
+# we must "normalize" them, i.e., get rid of 1-cycles
+def norm(perm: dict[int, int]):
+    return {k: perm[k] for k in perm if perm[k] != k}
+
 @given(gen_perm())
 def test_inversion(perm):
-    assert str_to_perm(perm_to_str(perm), 6) == perm
+    assert norm(str_to_perm(perm_to_str(perm))) == norm(perm)
 
 test_inversion()
 
-assert simplify('(1 2 3 4)', 4) == '(1 2 3 4)'
-assert simplify('(1 3 4)(1 2)(3 4)', 4) == '(1 2 3)'
-assert simplify('(1 3)(1 2)(3 4)', 4) == '(1 2 3 4)'
+assert evaluate('(1 2 3 4)') == '(1 2 3 4)'
+assert evaluate('(1 3 4)(1 2)(3 4)') == '(1 2 3)'
+assert evaluate('(1 3)(1 2)(3 4)') == '(1 2 3 4)'
